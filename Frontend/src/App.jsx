88f197d7de221da6b025542a6bfc37a40833e158
pkumavat <punamkumavat01@gmail.com>
2024-06-23 14:35:36 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,54 +1,34 @@
-import React, { useState } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import SignUp from './components/SignUp/SignUp';
 import Login from './components/Login/Login';
 import UserProfilePage from './pages/UserProfile/UserProfilePage';
-import { UserProvider } from './contexts/UserContext'
 import TestRide from './pages/TestRide/TestRide';
 import DigitalShowroom from './pages/DigitalShowroom/DigitalShowroom';
 import Home from './pages/Home';
 import PLP from './pages/PLP/PLP';
 import PDP from './pages/PDP';
-import Vehicles from './pages/Vehicles/Vehicles';
 import Blogs from './pages/Blogs/Blogs';
 import PreBooking from './pages/PreBooking/PreBooking'
 const App = () => {
-  const location = useLocation();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // Function to set login status
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
   return (
-      <div className="App">
-
-        {<Navbar />}
-        
-        <Routes >
-          <Route path="/testride" element={<TestRide />} />
-          <Route path="/showroom" element={<DigitalShowroom />} />
-          {/*<Route path="/" element={<Home />} />*/}
-          <Route path="/cars" element={<PLP />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/cars/:id" element={<PDP />} />
-          <Route path="/profile" element={<UserProfilePage />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path='/prebooking/:id' element={<PreBooking />}/>
-        </Routes>
-        {/*<SignUp />
-      {!isLoggedIn ? (
-          <Login onLoginSuccess={handleLogin} /> 
-        ) : (
-          <UserProfilePage />
-        )}*/}
-        {/*<AppRoutes location={location} key={location.pathname} />*/}
-
-      </div>
+    <div className="App">
+      <Navbar />
+      <Routes >
+        <Route path="/testride" element={<TestRide />} />
+        <Route path="/showroom" element={<DigitalShowroom />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<PLP />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cars/:id" element={<PDP />} />
+        <Route path="/profile" element={<UserProfilePage />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path='/prebooking/:id' element={<PreBooking />} />
+      </Routes>
+    </div>
   );
 };
 
